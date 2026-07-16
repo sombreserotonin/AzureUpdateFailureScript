569012866e7757eb299f7ef320405ff46317b211
Cristian Lustri <49113582+Luzzle@users.noreply.github.com>
2026-07-16 15:23:54 +1000

    Basic metrics (CPU and Memory)

--- src/v2/AzUpdateMgr-HostScript-Windows.ps1
+++ src/v2/AzUpdateMgr-HostScript-Windows.ps1
@@ -6,13 +6,24 @@
     Version: 1.0
 #>
 
-$results = @() -as [System.Object[]]
-# === Report CPU Usage ===
 
+# Initialize results array as empty array of objects to allow array items of multiple different types.
+$results = @() -as [System.Object[]]
+
+# === results[0] - CPU Usage ===
 $cpuUsage = (Get-WmiObject -Class Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
 $results += $cpuUsage
 
-$results += 100
-$results += "test string"
+# === results[1] - Total Provisioned Memory ===
+$memoryTotal = (Get-WmiObject -Class WIN32_OperatingSystem).TotalVisibleMemorySize
+$results += $memoryTotal
+
+# === results[2] - Total Used Memory ===
+$memoryFree = (Get-WmiObject -Class WIN32_OperatingSystem).FreePhysicalMemory
+$memoryUsed = $memoryTotal - $memoryFree
+$results += $memoryUsed
+
+# === results[3] - Total Provisioned Memory ===
+
 
 Write-Host ($results | ConvertTo-Json)
