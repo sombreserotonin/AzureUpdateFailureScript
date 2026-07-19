cfef8020299a056498a30e0662116aa71b390985
Cristian Lustri <49113582+Luzzle@users.noreply.github.com>
2026-07-19 13:11:08 +1000

    Bug fixed Windows Update reporting and implemented reporting for extensions on disk.

--- src/v2/AzUpdateMgr-HostScript-Windows.ps1
+++ src/v2/AzUpdateMgr-HostScript-Windows.ps1
@@ -24,25 +24,77 @@ $memoryUsed = $memoryTotal - $memoryFree
 $results += $memoryUsed
 
 # === results[3] - WUpdate Error Codes ===
+#TODO - Re-write this
+
 $Since = (Get-Date).AddHours(-24)
 $errorsRaw = @() -as [System.Object[]]
 
-Get-WinEvent -LogName Microsoft-Windows-WindowsUpdateClient/Operational |
-Where-Object {
-    $_.LevelDisplayName -eq "Error" -and
-    $_.TimeCreated -ge $Since
-} |
-ForEach-Object { 
-	$_.Message | Select-String -Pattern '0x[0-9A-Fa-f]{8}' -AllMatches |
-	ForEach-Object {
-		$errorCode = $_.Matches.Value
+$events = Get-WinEvent -LogName Microsoft-Windows-WindowsUpdateClient/Operational -ErrorAction SilentlyContinue
 
-		if ($errorsRaw -notcontains $errorCode){
-			$errorsRaw += $errorCode
+if ($events){
+	$events | Where-Object {
+		$_.LevelDisplayName -eq "Error" -and
+		$_.TimeCreated -ge $Since
+	} |
+	ForEach-Object { 
+		$_.Message | Select-String -Pattern '0x[0-9A-Fa-f]{8}' -AllMatches |
+		ForEach-Object {
+			$errorCode = $_.Matches.Value
+
+			if ($errorsRaw -notcontains $errorCode){
+				$errorsRaw += $errorCode
+			}
 		}
 	}
+	$results += ($errorsRaw | ConvertTo-Json)
+}else {
+	$results += ""
 }
 
-$results += ($errorsRaw | ConvertTo-Json)
+
+# === results[4] - Extensions on disk === 
+$extensions = @() -as [System.Object[]]
+
+$extensionsDir = "C:\Packages\Plugins"
+$extensionsDirExists = Test-Path -Path $extensionsDir
+
+if ($false -eq $extensionsDirExists){
+	$results += "NoDirFolder"
+}else{
+	$dirs = Get-ChildItem -Path $extensionsDir -Directory
+	
+	foreach ($dir in $dirs){
+		$extensionName = $dir.Name
+		
+		foreach ($versionNum in (Get-ChildItem -Path $dir.FullName -Name)){
+			$statusDir = "C:\Packages\Plugins\$extensionName\$versionNum\Status"
+			$statusDirExists = Test-Path -Path $statusDir
+
+			if ($false -eq $statusDirExists){
+				$extensions += @{
+					name = $extensionName
+					version = $versionNum.ToString()
+					status = "Invalid"
+				}
+			}else{
+				$currentStatusFile = (Get-ChildItem -Path $statusDir -Filter "*.status" | Sort-Object LastWriteTime -Descending)[0].Name
+				$statusJSON = (Get-Content -Raw -Path "$statusDir/$currentStatusFile") | ConvertFrom-Json
+				$status = $statusJSON[0].status.status
+
+				$extensions += @{
+					name = $extensionName
+					version = $versionNum.ToString()
+					status = $status
+				}
+			}
+		}
+	}
+
+	if ($extensions.Count -eq 0){
+		$results += "NoExtensions"
+	}else{
+		$results += ConvertTo-Json $extensions
+	}
+}
 
 Write-Host ($results | ConvertTo-Json)
